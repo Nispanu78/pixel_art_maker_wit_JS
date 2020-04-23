520c1d895647f0409a5a1e95d09cdcce79c795d1
Nispanu78 <61283520+Nispanu78@users.noreply.github.com>
2020-04-23 09:11:11 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,34 +1,3 @@
-// let color = document.getElementById('colorPicker');
-// let table = document.getElementById('pixelCanvas');
-// let sizePicker = document.getElementById('sizePicker');
-// let height = document.getElementById('inputHeight').value;
-// let width = document.getElementById('inputWidth').value;
-// makeGrid(height, width);
-// sizePicker.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     let height = document.getElementById('inputHeight').value;
-//     let width = document.getElementById('inputWidth').value;
-//     table.firstElementChild.remove();
-//     makeGrid(height, width);
-// });
-//
-// function makeGrid(height, width) {
-//     for (let i = 0; i < height; i++) {
-//         let row = table.insertRow(i);
-//         for (let j = 0; j < width; j++) {
-//             let cell = row.insertCell(j);
-//             cell.addEventListener('click', (e) => {
-//                 cell.style.backgroundColor = color.value;
-//             });
-//         }
-//     }
-// }
-//
-// Select color input
-// Select size input
-
-// set relevant variables
-//
 var sizePicker = document.querySelector('sizePicker');
 var table = document.querySelector('pixelCanvas');
 var height = document.querySelector('inputHeight').value;
