df771c82548cfa20b44695b20e2788fcb18b9c0e
Nispanu78 <61283520+Nispanu78@users.noreply.github.com>
2020-04-27 15:57:01 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,7 +6,7 @@ let height = document.getElementById('inputHeight').value;
 let width = document.getElementById('inputWidth').value;
 
 
-// an event listern is added to listen when a user submits a form
+// an event listenern is added to listen when a user submits a form
 sizePicker.addEventListener('click', function (event) {
     event.preventDefault();
     let height = document.getElementById('inputHeight').value;
@@ -15,7 +15,7 @@ sizePicker.addEventListener('click', function (event) {
     makeGrid(height, width);
 });
 
-// function that builds the grid according to the data submitted by the user.
+// function that builds the grid according to the data submitted by the user
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
         let row = canvas.insertRow(i);
