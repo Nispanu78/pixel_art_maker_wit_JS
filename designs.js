4b1098e039e82934d3d473917c6e4107fae69ea2
Nispanu78 <61283520+Nispanu78@users.noreply.github.com>
2020-04-27 15:55:30 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,30 +1,33 @@
-var sizePicker = document.querySelector('sizePicker');
-var table = document.querySelector('pixelCanvas');
-var height = document.querySelector('inputHeight').value;
-var width = document.querySelector('inputWidth').value;
-var color = document.querySelector('colorPicker');
+// variables to be used
+let color = document.getElementById('colorPicker');
+let canvas = document.getElementById('pixelCanvas');
+let sizePicker = document.getElementById('sizePicker');
+let height = document.getElementById('inputHeight').value;
+let width = document.getElementById('inputWidth').value;
 
-// When size is submitted by the user, call makeGrid()
-makeGrid(height, width);
 
 // an event listern is added to listen when a user submits a form
-sizePicker.addEventListener('click', function (respondToTheClick) {
-    respondToTheClick.preventDefault();
+sizePicker.addEventListener('click', function (event) {
+    event.preventDefault();
     let height = document.getElementById('inputHeight').value;
     let width = document.getElementById('inputWidth').value;
+    canvas.firstElementChild.remove();
     makeGrid(height, width);
 });
 
 // function that builds the grid according to the data submitted by the user.
-
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
-        let row = table.insertRow(i);
+        let row = canvas.insertRow(i);
         for (let j = 0; j < width; j++) {
             let cell = row.insertCell(j);
-            cell.addEventListener('click', (respondToTheClick) {
+// an event listern is added to listen when a user colors a cell
+            cell.addEventListener('click', function (respondToTheClick)  {
                 cell.style.backgroundColor = color.value;
-                });
-            }
+            });
         }
     }
+}
+// // When size is submitted by the user, call makeGrid()
+makeGrid(height, width);
+
