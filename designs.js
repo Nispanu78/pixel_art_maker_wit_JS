b5e65c9673db2931216f40eaf05f2fd9c729073b
Nispanu78 <61283520+Nispanu78@users.noreply.github.com>
2020-04-28 12:39:02 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,7 +6,7 @@ const height = document.getElementById('inputHeight').value;
 const width = document.getElementById('inputWidth').value;
 
 
-// an event listern is added to listen when a user submits a form
+// an event listener is added to listen when a user submits a form
 $sizePicker.addEventListener('submit', function(event) {
       // get input data and draw grid
       event.preventDefault();
@@ -17,13 +17,13 @@ $sizePicker.addEventListener('submit', function(event) {
       makeGrid(width, height);
     })
 
-// function that builds the grid according to the data submitted by the user.
+// function that builds the grid according to the data submitted by the user
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
         let row = canvas.insertRow(i);
         for (let j = 0; j < width; j++) {
             let cell = row.insertCell(j);
-// an event listern is added to listen when a user colors a cell
+// an event listener is added to listen when a user colors a cell
             cell.addEventListener('click', function (respondToTheClick)  {
                 cell.style.backgroundColor = color.value;
             });
